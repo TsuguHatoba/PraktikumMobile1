bc11a4fe1ba61560c5468b069a688205d2bc26d6
Muhammad Fadillah Hassan <2010817110005@mhs.ulm.ac.id>
2022-03-19 04:54:34 +0800

    Update PRAK101.kt

--- Modul1/PRAK101.kt
+++ Modul1/PRAK101.kt
@@ -1,14 +1,14 @@
 import kotlin.random.Random
 
 fun main() {
-    println("Waktu Sekarang: ")
+    print("Waktu Sekarang: ")
     val waktu = readLine()
-    println("Nama Anda: ")
+    print("Nama Anda: ")
     val nama = readLine()
-    println("Umur Anda: ")
+    print("Umur Anda: ")
     val umur = readLine()
 
-    println("Suhu Anda: ")
+    print("Suhu Anda: ")
     var suhu = readLine()
     val valueRandom =  String.format("%.2f",Random.nextDouble(suhu!!.toInt().toDouble(), 37.00))
     println("Selamat Pagi, $nama. Umur anda $umur Tahun \nSuhu Tubah anda $valueRandom derajat Celcius")
